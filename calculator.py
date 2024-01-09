4c5f59ad3c4228b2d5cf27d263a5f3d802747fc6
MicklePickl <154372950+MicklePickl@users.noreply.github.com>
2024-01-09 15:39:15 -0500

    Delete calculatorV0.1.py
    Replaced CalculatorV1 for a more favorable version

--- calculator.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-
-First_number = input("First number?: ")
-Second_number = input("Second number?: ")
-sum = int(First_number) + int(Second_number)
-print(sum)
-time.sleep(3)
-os.startfile('calculatorhub.py')
